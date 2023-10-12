240bdd46add1117d4d735a79d6d5893b2516b8fa
Ori-hub-code <uic3883@gmail.com>
2023-10-12 09:43:52 +0900

    물풍선 프리펩 오류 수정
    블럭에 닿을 시, 물줄기가 아주 잠깐 보이는 오류 수정 (애니메이션 수정)

--- crazy/Assets/Script/Water.cs
+++ crazy/Assets/Script/Water.cs
@@ -12,99 +12,6 @@ public class Water : MonoBehaviour
     public GameObject[] leftWater;
     public GameObject[] rightWater;
 
-    RaycastHit2D rayHit;
-
-
-    void Update()
-    {
-        // Ray
-        switch (gameObject.tag)
-        {
-            case "upWater":
-                for (int i = Array.IndexOf(upWater, gameObject); i < upWater.Length; i++)
-                {
-                    Debug.DrawRay(transform.position, Vector3.up * 0.7f, new Color(0, 1, 0));
-                    rayHit = Physics2D.Raycast(transform.position, Vector3.up, 0.7f, LayerMask.GetMask("Block"));
-                }
-
-                Invoke("BackCondition", 0.5f);
-                break;
-            case "downWater":
-                for (int i = Array.IndexOf(downWater, gameObject); i < downWater.Length; i++)
-                {
-                    Debug.DrawRay(transform.position, Vector3.down * 0.7f, new Color(0, 1, 0));
-                    rayHit = Physics2D.Raycast(transform.position, Vector3.down, 0.7f, LayerMask.GetMask("Block"));
-
-                }
-
-                Invoke("BackCondition", 0.5f);
-                break;
-            case "leftWater":
-                for (int i = Array.IndexOf(leftWater, gameObject); i < leftWater.Length; i++)
-                {
-                    Debug.DrawRay(transform.position, Vector3.left * 0.7f, new Color(0, 1, 0));
-                    rayHit = Physics2D.Raycast(transform.position, Vector3.left, 0.7f, LayerMask.GetMask("Block"));
-                }
-
-                Invoke("BackCondition", 0.5f);
-                break;
-            case "rightWater":
-                for (int i = Array.IndexOf(rightWater, gameObject); i < rightWater.Length; i++)
-                {
-                    Debug.DrawRay(transform.position, Vector3.right * 0.7f, new Color(0, 1, 0));
-                    rayHit = Physics2D.Raycast(transform.position, Vector3.right, 0.7f, LayerMask.GetMask("Block"));
-                }
-
-                Invoke("BackCondition", 0.5f);
-                break;
-        }
-
-        // Á¶°Ç¹® 
-        if (rayHit.collider != null)
-        {
-            switch (gameObject.tag)
-            {
-                case "upWater":
-                    for (int i = Array.IndexOf(upWater, gameObject) + 1; i < upWater.Length; i++)
-                    {
-                        upWater[i].SetActive(false);
-                        Debug.Log(i + " up");
-                    }
-
-                    Invoke("BackCondition", 0.5f);
-                    break;
-                case "downWater":
-                    for (int i = Array.IndexOf(downWater, gameObject) + 1; i < downWater.Length; i++)
-                    {
-                        downWater[i].SetActive(false);
-                        Debug.Log(i + " down");
-
-                    }
-
-                    Invoke("BackCondition", 0.5f);
-                    break;
-                case "leftWater":
-                    for (int i = Array.IndexOf(leftWater, gameObject) + 1; i < leftWater.Length; i++)
-                    {
-                        leftWater[i].SetActive(false);
-                        Debug.Log(i + " left");
-                    }
-
-                    Invoke("BackCondition", 0.5f);
-                    break;
-                case "rightWater":
-                    for (int i = Array.IndexOf(rightWater, gameObject) + 1; i < rightWater.Length; i++)
-                    {
-                        rightWater[i].SetActive(false);
-                        Debug.Log(i + " right");
-                    }
-
-                    Invoke("BackCondition", 0.5f);
-                    break;
-            }
-        }
-    }
-
     void BackCondition()
     {
         switch (gameObject.tag)
@@ -139,7 +46,46 @@ public class Water : MonoBehaviour
     void OnTriggerEnter2D(Collider2D obj)  {
         if (obj.gameObject.tag == "Block")
         {
-            gameObject.SetActive(false);
+            switch (gameObject.tag)
+            {
+                case "upWater":
+                    for (int i = Array.IndexOf(upWater, gameObject); i < upWater.Length; i++)
+                    {
+                        upWater[i].SetActive(false);
+                        Debug.Log(i + " up");
+                    }
+
+                    Invoke("BackCondition", 0.5f);
+                    break;
+                case "downWater":
+                    for (int i = Array.IndexOf(downWater, gameObject); i < downWater.Length; i++)
+                    {
+                        downWater[i].SetActive(false);
+                        Debug.Log(i + " down");
+
+                    }
+
+                    Invoke("BackCondition", 0.5f);
+                    break;
+                case "leftWater":
+                    for (int i = Array.IndexOf(leftWater, gameObject); i < leftWater.Length; i++)
+                    {
+                        leftWater[i].SetActive(false);
+                        Debug.Log(i + " left");
+                    }
+
+                    Invoke("BackCondition", 0.5f);
+                    break;
+                case "rightWater":
+                    for (int i = Array.IndexOf(rightWater, gameObject); i < rightWater.Length; i++)
+                    {
+                        rightWater[i].SetActive(false);
+                        Debug.Log(i + " right");
+                    }
+
+                    Invoke("BackCondition", 0.5f);
+                    break;
+            }
 
         }
     }
