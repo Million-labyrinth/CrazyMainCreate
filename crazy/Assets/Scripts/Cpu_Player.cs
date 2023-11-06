3b3237254d4aa5b7d85e59671cf288b896826720
HongYeongHo <145641517+HongYeongHo@users.noreply.github.com>
2023-11-06 17:47:37 +0900

    배회코드작성하기

--- crazy/Assets/Scripts/Cpu_Player.cs
+++ crazy/Assets/Scripts/Cpu_Player.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Cpu_Player : MonoBehaviour
@@ -10,14 +8,29 @@ public class Cpu_Player : MonoBehaviour
 
     private void Update()
     {
-        if(Vector2.Distance(transform.position, target.position) > minimumDistance)
+        TrackTarget(target);
+    }
+
+    public void TrackTarget(Transform target)
+    {
+        // 타겟과 플레이어의 위치 차이 계산
+        Vector3 direction = target.position - transform.position;
+
+        // 대각선 이동 제한
+        float diagonalLimit = Mathf.Sqrt(2) / 2;
+        if (Mathf.Abs(direction.x) > minimumDistance && Mathf.Abs(direction.z) > minimumDistance)
         {
-            transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+            if (Mathf.Abs(direction.x) > Mathf.Abs(direction.z))
+            {
+                direction.z = 0;
+            }
+            else
+            {
+                direction.x = 0;
+            }
         }
-        else
-        {
-            //attack code
-        }
-        
+
+        // 이동 코드 작성
+        transform.Translate(direction.normalized * speed * Time.deltaTime);
     }
 }
