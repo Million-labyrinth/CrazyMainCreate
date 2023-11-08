d93ee8207a162f3c6b25638aa62d4df8ca90e97f
HongYeongHo <145641517+HongYeongHo@users.noreply.github.com>
2023-11-08 16:43:50 +0900

    asdfas
    sadfsadf

--- crazy/Assets/Scripts/Cpu_Player.cs
+++ crazy/Assets/Scripts/Cpu_Player.cs
@@ -3,32 +3,42 @@ using UnityEngine;
 public class Cpu_Player : MonoBehaviour
 {
     Rigidbody2D rigid;
-    public int nextMove;//다음 행동지표를 결정할 변수
-    Vector3 endPosition;
+    Vector2[] waypoints; // 목적지 좌표 배열
+    int currentWaypointIndex = 0; // 현재 목적지 인덱스
+    public float moveSpeed = 3.0f; // 몬스터의 이동 속도
 
-    // Start is called before the first frame update
-    private void Awake()
+    void Start()
     {
-        rigid = GetComponent<Rigidbody2D>();
-        Think();
+        rigid = GetComponent <Rigidbody2D>();
 
+        // 목적지 좌표 배열 초기화
+        waypoints = new Vector2[] {
+            new Vector2(-1, 0),
+            new Vector2(3, 0),
+            new Vector2(3, 3),
+            new Vector2(3, 0)
+        };
+
+        // 초기 이동 방향 설정
+        SetNextWaypoint();
     }
 
-
-    // Update is called once per frame
     void FixedUpdate()
     {
-        rigid.velocity = endPosition; //nextMove 에 0:멈춤 -1:왼쪽 1:오른쪽 으로 이동 
+        // 몬스터를 현재 목적지로 이동시킴
+        Vector2 direction = (waypoints[currentWaypointIndex] - (Vector2)transform.position).normalized;
+        rigid.velocity = direction * moveSpeed;
+
+        // 목적지에 도착하면 다음 목적지로 설정
+        if (Vector2.Distance(transform.position, waypoints[currentWaypointIndex]) < 0.1f)
+        {
+            SetNextWaypoint();
+        }
     }
 
-
-    void Think()
-    {//몬스터가 스스로 생각해서 판단 (-1:왼쪽이동 ,1:오른쪽 이동 ,0:멈춤  으로 3가지 행동을 판단)
-
-        //Random.Range : 최소<= 난수 <최대 /범위의 랜덤 수를 생성(최대는 제외이므로 주의해야함)
-        nextMove = Random.Range(-1, 2);
-
-        //Think(); : 재귀함수 : 딜레이를 쓰지 않으면 CPU과부화 되므로 재귀함수쓸 때는 항상 주의 ->Think()를 직접 호출하는 대신 Invoke()사용
-        Invoke("Think", 5); //매개변수로 받은 함수를 5초의 딜레이를 부여하여 재실행 
+    void SetNextWaypoint()
+    {
+        // 다음 목적지로 설정하고 인덱스를 업데이트
+        currentWaypointIndex = (currentWaypointIndex + 1) % waypoints.Length;
     }
 }
