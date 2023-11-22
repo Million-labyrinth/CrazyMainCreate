cfa1bf38601e501a7c99c94339e50d56c372f156
05 <145641517+HongYeongHo@users.noreply.github.com>
2023-11-23 02:32:04 +0900

    ai 코드수정(미완)

--- crazy/Assets/Scripts/monsterAi.cs
+++ crazy/Assets/Scripts/monsterAi.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Burst.CompilerServices;
 using UnityEngine;
+using static UnityEngine.RuleTile.TilingRuleOutput;
 
 public class monsterAi : MonoBehaviour
 {
@@ -11,10 +13,18 @@ public class monsterAi : MonoBehaviour
     private Vector3 targetPosition;
     private float moveSpeed = 2.0f;
 
+    List<Vector2> hitPoints = new List<Vector2>();
+
+    Vector2 uphitPoint;
+    Vector2 downhitPoint;
+    Vector2 lefthitPoint;
+    Vector2 righthitPoint;
+    LayerMask layerMask;
     public Animator anim;
 
     void Awake()
     {
+        layerMask = LayerMask.GetMask("Block") | LayerMask.GetMask("MoveBlock") | LayerMask.GetMask("Object");
         // 시작 시 초기 위치 설정
         anim = GetComponent<Animator>();
         targetPosition = GetRandomPosition();
@@ -24,16 +34,16 @@ public class monsterAi : MonoBehaviour
     {
         
         //이동 애니메이션
-        if (transform.position.y > GetRandomPosition().y)
-        {
-            anim.SetBool("is_up", true);
-            anim.SetBool("is_down", false);
-        }
-        else if (transform.position.y < GetRandomPosition().y)
-        {
-            anim.SetBool("is_down", true);
-            anim.SetBool("is_up", false);
-        }
+        //if (transform.position.y > GetRandomPosition().y)
+        //{
+        //    anim.SetBool("is_up", true);
+        //    anim.SetBool("is_down", false);
+        //}
+        //else if (transform.position.y < GetRandomPosition().y)
+        //{
+        //    anim.SetBool("is_down", true);
+        //    anim.SetBool("is_up", false);
+        //}
         // 현재 위치에서 목표 위치로 이동
         transform.position = Vector3.MoveTowards(transform.position, targetPosition, moveSpeed * Time.deltaTime);
         // 만약 목표 위치에 도달했다면, 다른 랜덤 위치를 선택
@@ -44,52 +54,75 @@ public class monsterAi : MonoBehaviour
         AIRay();
     }
 
-    Vector3 GetRandomPosition()
+    Vector2 GetRandomPosition()
     {
-        // 랜덤하게 x 또는 y 좌표를 선택
-        bool chooseX = Random.Range(0, 2) == 0; // true면 x, false면 y
-        float randomX = chooseX ? Random.Range(xMin, xMax) : transform.position.x;
-        float randomY = chooseX ? transform.position.y : Random.Range(yMin, yMax);
+        Vector2 farthestPoint = Vector2.zero;
+        Vector2 secondFarthestPoint = Vector2.zero;
 
-        return new Vector3(randomX, randomY, 0);
+        hitPoints.Sort((a, b) => Vector2.Distance(transform.position, b).CompareTo(Vector2.Distance(transform.position, a)));
+
+        // 가장 먼 값과 두 번째로 먼 값을 구함
+        if (hitPoints.Count >= 1)
+        {
+            farthestPoint = hitPoints[0];
+        }
+        if (hitPoints.Count >= 2)
+        {
+            secondFarthestPoint = hitPoints[1];
+        }
+
+        Vector2 selectedPoint;
+
+        if (Random.Range(0, 2) == 0)
+        {
+            selectedPoint = farthestPoint;
+        }
+        else
+        {
+            selectedPoint = secondFarthestPoint;
+        }
+
+        return selectedPoint;
     }
     void AIRay()
     {
+        hitPoints.Clear();
         // 충돌 체크
-        Debug.DrawRay(transform.position + new Vector3(-0.3f, 0.45f, 0), Vector3.right * 0.5f, new Color(0, 1, 0));
-        Debug.DrawRay(transform.position + new Vector3(0.3f, -0.45f, 0), Vector3.left * 0.5f, new Color(0, 1, 0));
-        Debug.DrawRay(transform.position + new Vector3(-0.45f, 0.3f, 0), Vector3.down * 0.5f, new Color(0, 1, 0));
-        Debug.DrawRay(transform.position + new Vector3(0.45f, -0.3f, 0), Vector3.up * 0.5f, new Color(0, 1, 0));
-        RaycastHit2D upRayHit = Physics2D.Raycast(transform.position + new Vector3(-0.3f, 0.45f, 0), Vector3.right, 0.6f, LayerMask.GetMask("Block") | LayerMask.GetMask("MoveBlock") | LayerMask.GetMask("Object"));
-        RaycastHit2D downRayHit = Physics2D.Raycast(transform.position + new Vector3(0.3f, -0.45f, 0), Vector3.left, 0.6f, LayerMask.GetMask("Block") | LayerMask.GetMask("MoveBlock") | LayerMask.GetMask("Object"));
-        RaycastHit2D leftRayHit = Physics2D.Raycast(transform.position + new Vector3(-0.45f, 0.3f, 0), Vector3.down, 0.6f, LayerMask.GetMask("Block") | LayerMask.GetMask("MoveBlock") | LayerMask.GetMask("Object"));
-        RaycastHit2D rightRayHit = Physics2D.Raycast(transform.position + new Vector3(0.45f, -0.3f, 0), Vector3.up, 0.6f, LayerMask.GetMask("Block") | LayerMask.GetMask("MoveBlock") | LayerMask.GetMask("Object"));
+        Debug.DrawRay(transform.position + new Vector3(0, 0, 0), Vector3.up * 13f, new Color(0, 1, 0));
+        Debug.DrawRay(transform.position + new Vector3(0, 0, 0), Vector3.down * 13f, new Color(0, 1, 0));
+        Debug.DrawRay(transform.position + new Vector3(0, 0, 0), Vector3.left * 15f, new Color(0, 1, 0));
+        Debug.DrawRay(transform.position + new Vector3(0, 0, 0), Vector3.right * 15f, new Color(0, 1, 0));
+        RaycastHit2D upRayHit = Physics2D.Raycast(transform.position + new Vector3(0, 0, 0), Vector3.up, 13f, layerMask);
+        RaycastHit2D downRayHit = Physics2D.Raycast(transform.position + new Vector3(0, 0, 0), Vector3.down, 13f, layerMask);
+        RaycastHit2D leftRayHit = Physics2D.Raycast(transform.position + new Vector3(0, 0, 0), Vector3.left, 15f, layerMask);
+        RaycastHit2D rightRayHit = Physics2D.Raycast(transform.position + new Vector3(0, 0, 0), Vector3.right, 15f, layerMask);
 
-        // 충돌이 감지되면 새로운 위치 설정
         if (upRayHit.collider != null)
         {
-            yMax = transform.position.y;
-            targetPosition = GetRandomPosition();
-            yMax = 7.0f;
+            uphitPoint = upRayHit.point;
+            uphitPoint.y -= 1.0f;
+            hitPoints.Add(uphitPoint);
         }
-        else if (downRayHit.collider != null)
+        else if(downRayHit.collider != null)
         {
-            yMin = transform.position.y;
-            targetPosition = GetRandomPosition();
-            yMin = -7.0f;
+            downhitPoint = downRayHit.point;
+            downhitPoint.y += 1.0f;
+            hitPoints.Add(downhitPoint);
         }
-        else if (leftRayHit.collider != null)
+        else if(leftRayHit.collider != null)
         {
-            xMin = transform.position.x;
-            targetPosition = GetRandomPosition();
-            xMin = -8.0f;
+            lefthitPoint = leftRayHit.point;
+            lefthitPoint.x += 1.0f;
+            hitPoints.Add(lefthitPoint);
         }
-        else if (rightRayHit.collider != null)
+        else if(rightRayHit.collider != null)
         {
-            xMax = transform.position.x;
-            targetPosition = GetRandomPosition();
-            xMax = 7.0f;
+            righthitPoint = rightRayHit.point;
+            righthitPoint.y -= 1.0f;
+            hitPoints.Add(righthitPoint);
         }
+
+
     }
 
 }
