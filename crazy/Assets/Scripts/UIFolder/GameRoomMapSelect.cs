c2d3368e5ba46b4e726e1f6919c8037eb0725c7c
Peinoi <124995489+Peinoi@users.noreply.github.com>
2023-11-23 23:45:58 +0900

    맵 선택 수정

--- crazy/Assets/Scripts/UIFolder/GameRoomMapSelect.cs
+++ crazy/Assets/Scripts/UIFolder/GameRoomMapSelect.cs
@@ -6,50 +6,98 @@ using UnityEngine.UI;
 
 public class GameRoomMapSelect : MonoBehaviour
 
-    //미완성
+//미완성
 {
+    //맵 이름을 담기 위한 변수
+    public GameObject mapNameVillage;
+    public GameObject mapNameFactory;
+    public GameObject mapNameForest;
+    public GameObject mapNameRandom;
+
     public string[] RandomSceneNames;
 
-    public void ttv(string mapselect)
+    public void ttv()
     {
-        // mapselect에서 MapNameText를 가져와서 Text 컴포넌트를 얻음
-        GameObject mapNameTextObject = GameObject.Find(mapselect + "/MapNameText");
-
-        if (mapNameTextObject != null)
+        int ran = Random.Range(0, 2);
+        if (mapNameVillage.activeSelf == true || mapNameForest.activeSelf == true || mapNameFactory.activeSelf == true)
         {
-            Text mapNameTextComponent = mapNameTextObject.GetComponent<Text>();
-
-            if (mapNameTextComponent != null)
+            if (mapNameVillage.activeSelf == true)
             {
-                string currentMapName = mapNameTextComponent.text;
 
-                string sceneName = ""; // 넘어갈 씬의 이름을 지정해야 합니다.
-
-                switch (currentMapName)
-                {
-                    case "Village":
-                        sceneName = "BattelFieldVillage";
-                        break;
-                    case "Factory":
-                        sceneName = "BattleFieldFactory";
-                        break;
-                    case "PangLand":
-                        sceneName = "BattelFieldPangLand";
-                        break;
-                        // 추가적인 케이스들을 필요에 따라 지정하세요.
-                }
-
-                SceneManager.LoadScene(sceneName);
+                SceneManager.LoadScene("BattelFieldVillage");
+                Debug.Log("village map move");
             }
-            else
+            else if (mapNameForest.activeSelf == true)
             {
-                Debug.LogError(mapselect + "/MapNameText에 Text 컴포넌트가 없습니다.");
+                //SceneManager.LoadScene("BattleFieldForest");
             }
+            else if (mapNameFactory.activeSelf == true)
+            {
+                SceneManager.LoadScene("BattleFieldFactory");
+                Debug.Log("factory map move");
+            }
+
+        }
+        else if (mapNameRandom.activeSelf == true)
+        {
+            switch (ran)
+            {
+                case 0:
+                    SceneManager.LoadScene("BattelFieldVillage");
+                    Debug.Log("random village map move");
+                    break;
+                case 1:
+                    SceneManager.LoadScene("BattleFieldFactory");
+                    Debug.Log("random factory map move");
+                    break;
+                    // case 2:
+                    //     // SceneManager.LoadScene("BattleFieldForest");
+                    //     break;
+            }
+
         }
         else
         {
-            Debug.LogError(mapselect + "/MapNameText를 찾을 수 없습니다.");
+            return;
         }
+        // // mapselect에서 MapNameText를 가져와서 Text 컴포넌트를 얻음
+        // GameObject mapNameTextObject = GameObject.Find(mapselect + "/MapNameText");
+
+        // if (mapNameTextObject != null)
+        // {
+        //     Text mapNameTextComponent = mapNameTextObject.GetComponent<Text>();
+
+        //     if (mapNameTextComponent != null)
+        //     {
+        //         string currentMapName = mapNameTextComponent.text;
+
+        //         string sceneName = ""; // 넘어갈 씬의 이름을 지정해야 합니다.
+
+        //         switch (currentMapName)
+        //         {
+        //             case "Village":
+        //                 sceneName = "BattelFieldVillage";
+        //                 break;
+        //             case "Factory":
+        //                 sceneName = "BattleFieldFactory";
+        //                 break;
+        //             case "PangLand":
+        //                 sceneName = "BattelFieldPangLand";
+        //                 break;
+        //                 // 추가적인 케이스들을 필요에 따라 지정하세요.
+        //         }
+
+        //         SceneManager.LoadScene(sceneName);
+        //     }
+        //     else
+        //     {
+        //         Debug.LogError(mapselect + "/MapNameText에 Text 컴포넌트가 없습니다.");
+        //     }
+        // }
+        // else
+        // {
+        //     Debug.LogError(mapselect + "/MapNameText를 찾을 수 없습니다.");
+        // }
     }
 
     public void LoadRandomScene()
