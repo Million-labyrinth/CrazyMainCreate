5ec096f60d52238f506496b27a4147c068997285
GoodLooser <kis09188@naver.com>
2023-11-28 15:13:43 +0900

    배틀필드 -> 플레이어 룸으로 이동시 이름 초기화 고침

--- crazy/Assets/Scripts/UIFolder/TItlesOnPlayerRoom.cs
+++ crazy/Assets/Scripts/UIFolder/TItlesOnPlayerRoom.cs
@@ -7,15 +7,24 @@ public class TitlesOnPlayerRoom : MonoBehaviour
 {
     public GameObject playerRoom;
     public IDMgr idmgr;
+    public static bool shouldInitialize = true; // 초기화를 진행할지 여부를 결정하는 변수
 
     // public GameObject Player2;
-    private void Start()
-    {
-       idmgr.DeletePlayer1();
-        idmgr.DeletePlayer2();
-    }
+
     void Awake()
     {
+
+        if (shouldInitialize == true)
+        {
+            // 초기화 코드 실행
+            idmgr.DeletePlayer1();
+            idmgr.DeletePlayer2();
+
+
+            // 초기화가 끝났으므로 false로 변경
+            shouldInitialize = false;
+        }
+
         if (playerRoom != null)
         {
             int activatePlayerRoom = PlayerPrefs.GetInt("PlayerRoomState", 0);
