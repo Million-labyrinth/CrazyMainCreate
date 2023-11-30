ce64893a49d87425012335143c09c2bb24cdff88
HongYeongHo <145641517+HongYeongHo@users.noreply.github.com>
2023-11-30 17:49:32 +0900

    pvp 이긴사람 닉네임으로 변경

--- crazy/Assets/Scripts/UIFolder/UI_IDInput_Ingame.cs
+++ crazy/Assets/Scripts/UIFolder/UI_IDInput_Ingame.cs
@@ -7,12 +7,15 @@ public class UI_IDInput_Ingame : IDMgr
 {
     public Text textPlayer_1;
     public Text textPlayer_2;
-
+    public Text textPlayer_A;
+    public Text textPlayer_B;
     protected override void Awake()
     {
         base.Awake();           // ID ·Îµå
 
         textPlayer_1.text = strPlayer_1;
+        textPlayer_A.text = strPlayer_1;
         textPlayer_2.text = strPlayer_2;
+        textPlayer_B.text = strPlayer_2;
     }
 }
