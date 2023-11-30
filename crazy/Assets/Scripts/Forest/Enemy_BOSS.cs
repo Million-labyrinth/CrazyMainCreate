d72c344603eb517e16d907ff7f9be5bbdacf48bf
sun88110 <138852390+sun88110@users.noreply.github.com>
2023-11-30 16:09:23 +0900

    보스 패턴 제작중

--- crazy/Assets/Scripts/Forest/Enemy_BOSS.cs
+++ crazy/Assets/Scripts/Forest/Enemy_BOSS.cs
@@ -9,28 +9,28 @@ public class Enemy_BOSS : MonoBehaviour
 
    IEnumerator AttackDelay()
     {
+        yield return new WaitForSeconds(0.5f);
 
         while (Boss_Life = true)
         {
             int ran = UnityEngine.Random.Range(0, 3);
-
+            while (Boss_Life = true)
             if (ran == 0)
             {
-                
+              Boss_Attack[0].SetActive(true);
             }
             else if (ran == 1)
             {
-               
+               Boss_Attack[1].SetActive(true);
             }
             else if (ran == 2)
             {
-
+               Boss_Attack[2].SetActive(true);
             }
             else if (ran == 3)
             {
-
-            }
+               Boss_Attack[3].SetActive(true);
+             }
         }
-        yield return;  
     }
 }
