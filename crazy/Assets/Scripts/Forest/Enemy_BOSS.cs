9f91f3c206394bb940237a3805f4aa818efb8ceb
sun88110 <138852390+sun88110@users.noreply.github.com>
2023-11-30 15:55:09 +0900

    보스 스크립트 생각중

--- crazy/Assets/Scripts/Forest/Enemy_BOSS.cs
+++ crazy/Assets/Scripts/Forest/Enemy_BOSS.cs
@@ -4,9 +4,33 @@ using UnityEngine;
 
 public class Enemy_BOSS : MonoBehaviour
 {
-/*    IEnumerator AttackDelay()
+    public GameObject[] Boss_Attack; //보스 공격패턴 배열
+    public bool Boss_Life = true;
+
+   IEnumerator AttackDelay()
     {
-        Time.timeScale = 1f
+
+        while (Boss_Life = true)
+        {
+            int ran = UnityEngine.Random.Range(0, 3);
+
+            if (ran == 0)
+            {
+                
+            }
+            else if (ran == 1)
+            {
+               
+            }
+            else if (ran == 2)
+            {
+
+            }
+            else if (ran == 3)
+            {
+
+            }
+        }
         yield return;  
-    }*/
+    }
 }
